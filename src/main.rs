203da5e083af72111251b1dbefcb5f9ecaac5a1b
DanielGall500 <daniel.gallagher.js@gmail.com>
2024-11-29 17:09:18 +0100

    set relationship feature

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-mod mg; // Import the module
+mod mg; 
+use dotenv::dotenv;
+use std::env;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use crate::mg::*;
 
-    // Create a general graph
-    let general_graph = mg::GeneralGraph::new("neo4j://localhost:7687", "neo4j", "B1R2C3K4h5m6p7t8n9$$").await?;
-    general_graph.create_node("Person", "{name: 'Alice', age: 30}").await?;
+    dotenv().ok();
+    let secret_key = env::var("PASSWORD")
+        .expect("The database password must be set in a .env file.");
 
-    // Create a grammar graph
     let grammar_graph = mg::GrammarGraph::new(
         "neo4j://localhost:7687",
         "neo4j",
-        "B1R2C3K4h5m6p7t8n9$$",
+        secret_key.as_str(),
         "My Grammar Graph",
     )
     .await?;
-    grammar_graph.create_state("InitialState").await?;
+    grammar_graph.clear().await?;
+    grammar_graph.create_state("Sam").await?;
+    grammar_graph.create_state("Daniel").await?;
+    grammar_graph.connect_states("Sam", "Daniel", "omgtheybeinarelationship").await?;
+    grammar_graph.connect_states("Daniel", "Sam", "woaaahrelationship").await?;
 
     Ok(())
 }
--- src/mg.rs
+++ src/mg.rs
@@ -13,14 +13,38 @@ pub mod mg {
             Ok(Self{ graph })
         }
 
-        pub async fn create_node(&self, label: &str, properties: &str) -> Result<(), Box<dyn Error>> {
-            // Dynamically construct the query string with label and properties
-            // let query = format!("CREATE (p:{} {})", label, properties);
-            
-            // Execute the query
-            self.graph.run(query("CREATE (p: State { name: $name })").param("name", "Jack")).await?;
+        pub async fn create_node(&self, category: &str, label_id: &str, label_val: &str) -> Result<(), Box<dyn Error>> {
+            let create_node_query = format!("CREATE (p:{} {{ {}: \"{}\" }})", category, label_id, label_val);
+            self.graph.run(query(create_node_query.as_str())).await?;
             Ok(())
         }
+
+        pub async fn set_node_property(&self, node_type: &str, node_id_key: &str, node_id_val: &str, property_key: &str, property_val: &str) -> Result<(), Box<dyn Error>> {
+            let set_node_property_query = format!(
+                "MATCH (n:{} {{ {}: {} }}) SET n.{} = {} RETURN n",
+                node_type, node_id_key, node_id_val, property_key, property_val
+            );
+
+            self.graph.run(query(set_node_property_query.as_str())).await?;
+            Ok(())
+        }
+
+        pub async fn set_relationship(&self, cat_a: &str, cat_b: &str, node_a: &str, node_b: &str, rel: &str) -> Result<(), Box<dyn Error>> {
+            let set_relationship_query = format!(
+                "MATCH (a:{} {{ name: \"{}\" }}), (b:{} {{name: \"{}\" }})
+                CREATE (a)-[:{}]->(b)
+                RETURN a, b", cat_a, node_a, cat_b, node_b, rel);
+            self.graph.run(query(set_relationship_query.as_str())).await?;
+            Ok(())
+        }
+
+        pub async fn clear(&self) -> Result<(), Box<dyn Error>> {
+            let remove_all_query: &str = "MATCH (n) DETACH DELETE n";
+            self.graph.run(query(remove_all_query)).await?;
+            Ok(())
+        }
+
+
     }
 
     pub struct GrammarGraph {
@@ -39,8 +63,18 @@ pub mod mg {
         }
 
         pub async fn create_state(&self, name: &str) -> Result<(), Box<dyn Error>> {
-            let properties = format!("{{name: '{}'}}", name);
-            self.base.create_node("State", &properties).await?;
+            // let properties = format!("{{name: '{}'}}", name);
+            self.base.create_node("State", "name", name).await?;
+            Ok(())
+        }
+
+        pub async fn connect_states(&self, state_a: &str, state_b: &str, rel: &str) -> Result<(), Box<dyn Error>> {
+            self.base.set_relationship("State", "State", state_a, state_b, rel).await?;
+            Ok(())
+        }
+
+        pub async fn clear(&self) -> Result<(), Box<dyn Error>> {
+            self.base.clear().await?;
             Ok(())
         }
 
