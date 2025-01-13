f97383ec655d355ebf95d6bcec1dfd884fd4d226
Daniel Gallagher <daniel.gallagher.js@gmail.com>
2025-01-13 13:09:15 +0100

    improved node vis properties

--- ui/src/components/GraphVis.vue
+++ ui/src/components/GraphVis.vue
@@ -34,9 +34,38 @@ function reload() {
             serverUser: serverUsername,
             serverPassword: serverPassword
         },
+        visConfig: {
+          nodes: {
+              shape: 'circle',
+              size: 100
+          },
+          edges: {
+              arrows: {
+                  to: {enabled: true}
+              }
+          },
+          physics: {
+            enabled: true, // Enable physics simulation
+            solver: 'forceAtlas2Based', // Use the forceAtlas2 solver for better node spacing
+            forceAtlas2Based: {
+                gravitationalConstant: -100, // Change gravitational constant to pull nodes further apart
+                centralGravity: 0.01, // Adjust the central gravity to move nodes away from the center
+                springLength: 250, // Increase spring length to make nodes more spread out
+                springConstant: 0.05, // Decrease spring constant for less attractive force
+                damping: 0.4, // Adjust the damping to slow the node movement
+            },
+            barnesHut: {
+                theta: 0.5, // This is relevant if you're using barnesHut solver; controls accuracy vs. performance
+            },
+            stabilization: {
+                iterations: 1000, // Number of iterations for stabilization before the physics stop
+                updateInterval: 25, // Interval for updates in milliseconds
+            }
+          },
+        },
         labels: {
           State: {
-              label: "name"
+              label: "name",
           }
         },
         relationships: {
