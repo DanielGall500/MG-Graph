2ec844fcbb738cdfa781f557192d49c72b52d905
Daniel Gallagher <daniel.gallagher.js@gmail.com>
2025-01-22 16:00:36 +0100

    candidate search for LI decomp

--- src/parse/decomp.rs
+++ src/parse/decomp.rs
@@ -1,5 +1,7 @@
 use crate::parse::mg::LexicalItem;
 use std::collections::{HashMap, HashSet};
+use std::cmp::min;
+use std::iter::zip;
 
 pub struct Decomposer {
     mg: Vec<LexicalItem>
@@ -24,14 +26,84 @@ impl Decomposer {
 
     }
 
+    pub fn find_decomposition_candidates(&self, mg: &Vec<LexicalItem>) -> HashMap<String, Vec<(usize, f64)>> {
+        let affix_map: HashMap<String, HashSet<usize>> = self.get_affix_map(&mg);
+
+        let empty_li = LexicalItem {
+            morph: String::from(""),
+            bundle: Vec::new()
+        };
+
+        let mut candidate_map: HashMap<String, Vec<(usize, f64)>> = HashMap::new();
+        let mut avg_sim: f64;
+        let mut total_sim: f64;
+        let mut count: usize;
+        let mut li_similarity_vec: Vec<f64>;
+
+        for (affix, li_indices) in affix_map.iter() {
+
+            for i in li_indices.iter() {
+                if let Some(affix_li) = mg.get(i.clone()) {
+
+                    /* Bug: Includes similarity to itself. */
+                    li_similarity_vec = li_indices.iter()
+                    .map(|x| self.get_syntactic_similarity(mg.get(x.clone()).unwrap_or(&empty_li), affix_li)).collect();
+
+                    println!("Similarities of {}:", affix_li.morph);
+                    for s in &li_similarity_vec {
+                        println!("{}", s);
+                    }
+                    println!("----");
+
+                    total_sim = li_similarity_vec.iter().sum(); 
+                    count = li_similarity_vec.len();
+                    avg_sim = total_sim / count as f64;
+
+                    candidate_map.entry(affix.clone())
+                    .or_insert_with(Vec::new)
+                    .push((i.clone(), avg_sim));
+                }
+            }
+
+        }
+        candidate_map
+    }
+
+    pub fn get_syntactic_similarity(&self,  l1: &LexicalItem, l2: &LexicalItem) -> f64 {
+        let fb1: Vec<String> = l1.bundle.iter().map(|x| x.raw.clone()).collect();
+        let fb2: Vec<String> = l2.bundle.iter().map(|x| x.raw.clone()).collect();
+        let mut similarity: f64 = 0.0;
+        let n: usize;
+
+        let kronecker_delta = |x: bool| -> f64 {
+            x as u16 as f64
+        };
+
+        let is_active_feature_same = &fb1[0] == &fb2[0];
+        if is_active_feature_same {
+            n = min(fb1.len(), fb2.len());
+            let alpha: f64 = 2.0;
+            let mut f1: &String;
+            let mut f2: &String;
+            let mut wi: f64;
+
+            // start from the first non-active feature (the second overall feature)
+            for i in 1..n {
+                f1 = &fb1[i];
+                f2 = &fb2[i];
+                wi = (-alpha * ((i-1) as f64)).exp();
+                println!("Updating Similarity with weight {} and delta {}", wi, kronecker_delta(f1 == f2));
+                // does the n normalise to some extent?
+                similarity += (kronecker_delta(f1 == f2)) / n as f64;
+            }
+        }
+        similarity
+    }
+
     pub fn get_affix_map(&self, mg: &Vec<LexicalItem>) -> HashMap<String, HashSet<usize>> {
         // all morphemes in our MG
         let morphs: Vec<String> = mg.iter().map(|x| x.morph.to_string()).collect();
-        let mut pairs: Vec<(String, String)> = Vec::new();
-
         let mut affix_map: HashMap<String, HashSet<usize>> = HashMap::new();
-
-        let mut affixes: Vec<Affix> = Vec::new();
 
         /* TODO: OPTMISATION PROCESS */
         // all pairs in our MG
@@ -44,12 +116,12 @@ impl Decomposer {
                 // creates a dictionary which stores each affix
                 // and the lexical item index to which it is associated.
                 if !pre.is_empty() {
-                    affix_map.entry(pre)
+                    affix_map.entry(format!("{pre}-"))
                     .or_insert_with(HashSet::new)
                     .extend([i,j].iter());
                 }
                 if !suff.is_empty() {
-                    affix_map.entry(suff)
+                    affix_map.entry(format!("-{suff}"))
                     .or_insert_with(HashSet::new)
                     .extend([i,j].iter());
                 }
@@ -92,5 +164,23 @@ pub fn test_decompose_affix_finder(mg: &Vec<LexicalItem>) {
     println!("Affix Map:");
     for (key, values) in &affix_map {
         println!("{}: {:?}", key, values);
+    }
+
+    println!("Test Similarity Calculator");
+    if let Some(li1) = mg.get(1) {
+        if let Some(li2) = mg.get(2) {
+            let similarity = decomp.get_syntactic_similarity(&li1, &li2);
+            println!("Similarity between {} and {} is {}", li1.morph, li2.morph, similarity);
+        }
+    }
+
+    let candidate_map = decomp.find_decomposition_candidates(&mg);
+    println!("Candidate Map:");
+    for (key, values) in &candidate_map {
+        for (index, sim) in values.iter() {
+            if let Some(li) = mg.get(index.clone()) {
+                println!("Avg Sim of {} is {} for affix {}", li.morph, sim, key);
+            }
+        }
     }
 }
