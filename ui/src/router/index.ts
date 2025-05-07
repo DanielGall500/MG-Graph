c14bbddf449d7d00dee0dbdbc715a83c3a6df5c1
Daniel Gallagher <daniel.gallagher.js@gmail.com>
2025-05-07 23:03:59 +0200

    new routes

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 const getHomeViewPage = () => import('@/views/HomeView.vue')
 const getAboutViewPage = () => import('@/views/AboutView.vue')
+const getExamplesPage = () => import('@/views/ExamplesView.vue')
+const getMyGrammarsPage = () => import('@/views/MyMGsView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +15,16 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: getAboutViewPage
+    },
+    {
+      path: '/examples',
+      name: 'examples',
+      component: getExamplesPage
+    },
+    {
+      path: '/mygrammars',
+      name: 'mygrammars',
+      component: getMyGrammarsPage
     }
   ]
 })
