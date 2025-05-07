e5fe4ff9a69b2a908f4f324d21e7b99eb9223329
Daniel Gallagher <daniel.gallagher.js@gmail.com>
2025-05-07 20:41:15 +0200

    added about view

---
+++ ui/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>This is a simple application to visualize Neo4j data.</p>
+        <p>It uses Vue.js for the frontend and Neo4j for the backend.</p>
+        <p>To get started, check out the documentation on how to set up your environment.</p>
+    </div>
+</template>
+
